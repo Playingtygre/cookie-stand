859c948aa8d325313bdaf7b292d55c75b92eeda0
Playingtygre <kymiehsu123@gmail.com>
2017-08-17 18:21:11 -0700

    thursday

--- app.js
+++ app.js
@@ -49,13 +49,14 @@ var createTable = function(store) {
     tableData = document.createElement('td');
     tableData.innerText = stores[i].location;
     tableRow.appendChild(tableData);
-    for (var j = 0; j < timeOfDay.length; j++){
+    for (var j = 0; j < timeOfDay.length - 1; j++){
       tableData = document.createElement('td');
       tableData.innerText = stores[i].averageCookies[j];
       tableRow.appendChild(tableData);
     }
     var tableColumn = document.createElement('td');
     tableColumn.innerText = store[i].totalCookies;
+    console.log(store[i].totalCookies);
     tableRow.appendChild(tableColumn);
 
     tableBody.appendChild(tableRow);
@@ -66,14 +67,14 @@ var createTable = function(store) {
 console.log('hello');
 
 //totals box//// needs to pull data from all other hours and adds them
-
-var totalbox = function(store){
-  for ( var i = 0; i < store.length; i++){
-    var tableColumn = document.createElement('td');
-    tableColumn.innerText = store[i].totalCookies;
-    tableData.appendChild('tableColumn');
-  }
-};
+//
+// var totalbox = function(store){
+//   for ( var i = 0; i < store.length; i++){
+//     var tableColumn = document.createElement('td');
+//     tableColumn.innerText = store[i].totalCookies;
+//     tableData.appendChild('tableColumn');
+//   }
+// };
 // console.log('hello')
 
 new StorePlace (65, 23, 6.3, '1st and Pike');
@@ -81,6 +82,8 @@ new StorePlace (24, 3, 1.2, 'SeaTac Airport');
 new StorePlace (38, 11, 3.7, 'Seattle Center');
 new StorePlace (38, 20, 2.3, 'Capital Hill');
 new StorePlace (16, 2, 4.6, 'Alki');
+//new StorePlace (1,1,1,'park place')
+
 
 //calls the cookie method
 for (var k = 0; k < stores.length; k++){
@@ -97,18 +100,23 @@ submit.addEventListener('click', myFunction);
 function myFunction() {
   alert ('Thank you Pat for submitting');
 };
-////input the submit button to input data onto the rows
-function myCreateFunction() {
-    var table = document.getElementById("td");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-};
 
-function myDeleteFunction() {
-    document.getElementById('td').deleteRow(0);
+//creat a new elelt every time the button is clicked
+//take data from form, create a new store ojbect
+//append new store and call function to store.
+
+////input the submit button to input data onto the rows
+// function myCreateFunction() {
+//     var table = document.getElementById("td");
+//     var row = table.insertRow(0);
+//     var cell1 = row.insertCell(0);
+//     var cell2 = row.insertCell(1);
+//     cell1.innerHTML = "NEW CELL1";
+//     cell2.innerHTML = "NEW CELL2";
+// };
+//
+// function myDeleteFunction() {
+//  document.getElementById('td').deleteRow(0);
 
 /////
 // fucntion(putting arugments in fuction)
